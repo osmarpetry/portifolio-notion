d5ecc5f4ee1b65855c5466614b8ff3df47f13f2c
osmarpetry <osmarpetry@gmail.com>
2023-02-10 16:01:24 -0300

    update gtag script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -65,16 +65,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        strategy='afterInteractive'
-        src='https://www.googletagmanager.com/gtag/js?id=G-HYKNYWFN9Z'
-      >
+      <Script strategy='afterInteractive'>
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-HYKNYWFN9Z');
+            <!-- Google Tag Manager -->
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K8823D8');
+            <!-- End Google Tag Manager -->
       `}
       </Script>
       <Analytics />
