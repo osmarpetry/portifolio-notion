a99e46484878d86c97a86dadf3dd5b0789e37c9d
osmarpetry <osmarpetry@gmail.com>
2023-02-13 14:25:40 -0300

    update google tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -65,15 +65,18 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script
+        async
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-3STVN66PY5'
+      />
       <Script strategy='afterInteractive'>
         {`
-            <!-- Google Tag Manager -->
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K8823D8');
-            <!-- End Google Tag Manager -->
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3STVN66PY5');
       `}
       </Script>
       <Analytics />
